e667065372603733d225bc815a3e4fc5c6f8a3a0
April Cook <ocitypromo@gmail.com>
2021-07-15 20:58:26 -0500

    commit for js day 4 of 100

---
+++ playground/types.js
@@ -0,0 +1,16 @@
+const name = 'April';
+const middle = "Anne";
+const last = `Cook`;
+
+/* const sentence = 'she\'s so cool'; // escapes the symbol breaking the string
+const sentence2 = `she's so "cool"`;
+
+const song = `oh
+yeah
+
+I  love
+
+pizza!!!`;
+*/
+
+const hello = 'hello my name is ' + name + ". Nice to meet you";
